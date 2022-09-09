f580afd09fc9200bced6bc571b5a8e650805201e
nthduc <nguyenthaiduc0212@gmail.com>
2022-09-09 17:13:18 +0700

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { SearchIcon, ShoppingBagIcon, UserIcon } from "@heroicons/react/outline";
+
+function Header() {
+
+    const session = false;
+
+  return (
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE]">
+      {/* Logo Apple */}
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+            <Image
+              src="https://rb.gy/vsvv2o"
+              layout="fill"
+              objectFit="contain"
+              alt="logo"
+            />
+          </div>
+        </Link>
+      </div>
+      {/* End Logo Apple */}
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Product</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Business</a>
+      </div>
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+        <SearchIcon className="headerIcon" />
+      </div>
+      <Link href="/checkout">
+          <div className="relative cursor-pointer">
+            <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center
+            justify-center rounded-full bg-gradient-to-r from-pink-500 to-violin-500 text-[10px] text-white">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon"/>
+          </div>
+      </Link>
+      
+      {session ? (
+        <Image
+        src={
+        //   session.user?.image ||
+          "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+        }
+        alt=""
+        className="cursor-pointer rounded-full"
+        width={34}
+        height={34}
+        // onClick={() => signOut()}
+      />
+      ): (
+        <UserIcon 
+        className="headerIcon" 
+        // onClick={() => signIn()} 
+        />
+      )}
+    </header>
+  );
+}
+
+export default Header;
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const path = require('path')
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["rb.gy"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+module.exports = nextConfig
